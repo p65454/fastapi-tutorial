7d0d6fd18ac31b05f5f495ede2549ee70fb5a561
p65454 <p65454@gmail.com>
2024-04-30 05:01:00 +0000

    feat - add_redis_cache

---
+++ app/core/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+from app.core.config import config
+
+def init_logger() -> None:
+    logger = logging.getLogger("uvicorn")
+    logger.setLevel(config.LOG_LEVEL)
+
+    return logger
+
+logger = init_logger()
