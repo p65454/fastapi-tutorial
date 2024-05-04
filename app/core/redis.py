a79c01d371d492687830a63552540d9f5ea2d826
p65454 <p65454@gmail.com>
2024-05-04 02:32:24 +0000

    feat - add-redis-decorator

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,9 +1,12 @@
 import pickle
 from typing import Optional
+from functools import wraps
 
 from redis.asyncio import Redis
 
 from app.core.config import config
+from app.core.logger import logger
+
 
 class RedisCache:
     def __init__(self):
@@ -17,10 +20,10 @@ class RedisCache:
 
     async def close(self) -> None:
         await self.redis.close()
-    
+
     async def set(self, key: str, value: object, ttl: Optional[int] = None) -> None:
         await self.redis.set(key, pickle.dumps(value), ex=ttl)
-    
+
     async def get(self, key: str) -> object:
         value = await self.redis.get(key)
         return pickle.loads(value)
@@ -28,7 +31,31 @@ class RedisCache:
     async def exists(self, key: str) -> bool:
         return await self.redis.exists(key)
 
-def key_builder(*args) -> str:
-    return ":".join(map(str, args))
 
 redis_cache = RedisCache()
+
+
+class RedisCacheDecorator:
+    def __init__(self, ttl: int = 60):
+        self.ttl = ttl
+
+    def key_builder(self, *args) -> str:
+        return ":".join(map(str, args))
+
+    def __call__(self, func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            _key = self.key_builder(func.__name__, *args, *kwargs)
+
+            if await redis_cache.exists(_key):
+                logger.debug("Cache hit")
+                result = await redis_cache.get(_key)
+            else:
+                logger.debug("Cache miss")
+                result = await func(*args, **kwargs)
+                if result:
+                    await redis_cache.set(_key, result, ttl=self.ttl)
+
+            return result
+
+        return wrapper
